34df0b8e24cd88c45242ad7f0cc67ca6ca9558c4
carlitaRP <zs20006731@estudiantes.uv.mx>
2024-04-17 20:34:20 -0600

    update

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -18,9 +18,8 @@ export class Login2Component {
 
   onSubmit() {
     if (!this.usuario.matricula || !this.usuario.password) {
-      // Mostrar toast de campos vacíos
       this.showToast('blankFieldsToast');
-      return; // Detener el proceso de inicio de sesión
+      return; 
     }
 
     if (this.validateMatricula(this.usuario.matricula) && this.validatePassword(this.usuario.password)) {
@@ -28,35 +27,30 @@ export class Login2Component {
         .subscribe(
           (response) => {
             if (response.success) {
-              // Mostrar toast de autenticación exitosa
               this.showToast('successToast');
               // Redireccionar a la página de inicio
               this.router.navigate(['/inicio']); // Reemplaza '/inicio' con la ruta de tu página de inicio
             } else {
-              // Mostrar toast de autenticación fallida
               this.showToast('errorToast');
             }
           },
           (error) => {
-            // Mostrar toast de autenticación fallida
             this.showToast('errorToast');
           }
         );
     } else {
-      // Mostrar toast de autenticación fallida
       this.showToast('errorToast');
     }
   }
 
   validateMatricula(matricula: string): boolean {
-    // Validar la matrícula según los requisitos
-    const regex = /^A\d{2}\d{3}$/; // Matrícula: A + dos dígitos de año + tres dígitos aleatorios
+    // Validar la matrícula 
+    const regex = /^A\d{2}\d{3}$/; 
     return regex.test(matricula);
   }
 
   validatePassword(password: string): boolean {
-    // Validar la contraseña según las características de una contraseña segura
-    // Por ejemplo, al menos 8 caracteres, al menos una letra minúscula, al menos una letra mayúscula, al menos un número
+    // Validar  una contraseña segura
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
     return regex.test(password);
   }
